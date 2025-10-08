d6826ea28ae7e60b7f9d10e2c4446a0030bbab16
Mugara250 <mbonyumugara52@gmail.com>
2025-10-08 13:19:39 +0200

    database setup and connection

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
 
 app.get("/", (request, response) => {
   response.send(`Hello there. Greetings from port ${PORT}!`);
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started listening at port ${PORT}!`);
+mongoose.connect(MONGODB_URI).then(() => {
+  console.log("Database connected!");
+  app.listen(PORT, () => {
+    console.log(`Server started listening at port ${PORT}!`);
+  });
+}).catch((err) => {
+    console.log(`Failed database connection: ${err}`);
 });
